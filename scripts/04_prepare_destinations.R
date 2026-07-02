91171ea19eb44498d595625727a4881d240acb2f
Katherine Losada <katielosada@gmail.com>
2026-07-02 15:28:51 -0400

    Add header comment

--- scripts/04_prepare_destinations.R
+++ scripts/04_prepare_destinations.R
@@ -1,6 +1,8 @@
 source(here::here("scripts", "00_utils.R"))
 ensure_directories()
 
+# Converts destination coordinates to spatial points and saves for routing.
+
 destinations <- destinations_tbl %>%
   st_as_sf(coords = c("lon", "lat"), crs = 4326, remove = FALSE) %>%
   st_transform(study_crs)
